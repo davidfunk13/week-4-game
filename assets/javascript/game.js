ec50fb9ac1f02341a192e485ecb47c2512205051
Dave <davefunk135@gmail.com>
2017-12-14 02:06:51 -0800

    have added random number generators between one and twelve for each of the four crystals.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,12 @@ var chosenNumber = document.getElementById("number")
 // writes a random number between 19 and 120 to the div.
 chosenNumber.innerHTML = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
 };
+//Red Crystal random Value
+redValue =  Math.floor((Math.random() * 12) + 1);
+//blue crystal random value
+blueValue =  Math.floor((Math.random() * 12) + 1);
+//green crystal random value
+greenValue =  Math.floor((Math.random() * 12) + 1);
+//purple crystal random value
+purpleValue =  Math.floor((Math.random() * 12) + 1);
+console.log(redValue, blueValue, greenValue, purpleValue);
