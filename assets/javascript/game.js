31af64f933a479bb053aaf14e5b1a3816a9cb1b1
Dave <davefunk135@gmail.com>
2017-12-15 02:24:28 -0800

    trying some things with onclicks and working on adding the score together

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,45 @@
-$(document).ready(function() {
-    
-//selects "number" html div. Writes a random number between 19 and 120 to the div.
-var chosenNumber = $("#number").html(Math.floor((Math.random() * (120 - 19 + 1)) + 19));
-//Total score
-var totalScore = 0;
-console.log(totalScore);
+$(document).ready(function () {
+
+    //selects "number" html div. Writes a random number between 19 and 120 to the div.
+    var chosenNumber = $("#number").html(Math.floor((Math.random() * (120 - 19 + 1)) + 19));
+    //Total score
+    var totalScore = 0;
+    console.log(totalScore);
 
 
-function getRandomNumber() {
-    var randomNumber = Math.floor((Math.random() *12) + 1);
-    return randomNumber;
-};
-var randomNumber = getRandomNumber();
-console.log(randomNumber);
+    function getRandomNumber() {
+        var randomNumber = Math.floor((Math.random() * 12) + 1);
+        return randomNumber;
+    };
+    var randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
-// //Red Crystal random Value
-// randomRed =  Math.floor((Math.random() * 12) + 1);
-// //blue crystal random value
-// randomBlue =  Math.floor((Math.random() * 12) + 1);
-// //green crystal random value
-// randomGreen =  Math.floor((Math.random() * 12) + 1);
-// //purple crystal random value
-// randomPurple =  Math.floor((Math.random() * 12) + 1);
-//button values
-var redValue = getRandomNumber();
-var blueValue = getRandomNumber();
-var greenValue = getRandomNumber();
-var purpleValue = getRandomNumber();
+    //button values
+    var redValue = getRandomNumber();
+    var blueValue = getRandomNumber();
+    var greenValue = getRandomNumber();
+    var purpleValue = getRandomNumber();
 
-console.log(redValue, blueValue, greenValue, purpleValue);
+    console.log(redValue, blueValue, greenValue, purpleValue);
 
-//print total score to the page
-$("#score").html(totalScore);
-//button functionality
+    //print total score to the page
+    $("#score").html(totalScore);
+    //button functionality
+
+
+
+    //add values function
+
+    function addValues() {
+
+    }
+
+    $('#red').on("click", function () {
+        redAdd = parseInt(redValue) + parseInt(totalScore);
+        console.log(redAdd);
+        $('#score').html(parseInt(redAdd));
+     });
+    $('#blue').on("click", function () { console.log(blueValue) });
+    $('#green').on("click", function () { console.log(greenValue) });
+    $('#purple').on("click", function () { console.log(purpleValue) });
 });
