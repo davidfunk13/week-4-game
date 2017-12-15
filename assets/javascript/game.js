ca63f2c74a5ce9e44e70a85ede6e88daddd26a1a
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-14 22:19:32 -0800

    added some tweaks to random number and got rid of some variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,32 @@ var chosenNumber = $("#number").html(Math.floor((Math.random() * (120 - 19 + 1))
 //Total score
 var totalScore = 0;
 console.log(totalScore);
-//Red Crystal random Value
-randomRed =  Math.floor((Math.random() * 12) + 1);
-//blue crystal random value
-randomBlue =  Math.floor((Math.random() * 12) + 1);
-//green crystal random value
-randomGreen =  Math.floor((Math.random() * 12) + 1);
-//purple crystal random value
-randomPurple =  Math.floor((Math.random() * 12) + 1);
+
+
+function getRandomNumber() {
+    var randomNumber = Math.floor((Math.random() *12) + 1);
+    return randomNumber;
+};
+var randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+// //Red Crystal random Value
+// randomRed =  Math.floor((Math.random() * 12) + 1);
+// //blue crystal random value
+// randomBlue =  Math.floor((Math.random() * 12) + 1);
+// //green crystal random value
+// randomGreen =  Math.floor((Math.random() * 12) + 1);
+// //purple crystal random value
+// randomPurple =  Math.floor((Math.random() * 12) + 1);
 //button values
-var redValue = [randomRed];
-var blueValue = [randomBlue];
-var greenValue = [randomGreen];
-var purpleValue = [randomPurple];
+var redValue = getRandomNumber();
+var blueValue = getRandomNumber();
+var greenValue = getRandomNumber();
+var purpleValue = getRandomNumber();
 
 console.log(redValue, blueValue, greenValue, purpleValue);
 
 //print total score to the page
 $("#score").html(totalScore);
 //button functionality
-
-function red() {
-    $('red').val(redValue);
-}
-
 });
