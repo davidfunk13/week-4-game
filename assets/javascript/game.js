e6caa60b6946981697f851085d9cf31f32ac9edc
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-14 16:23:55 -0800

    added a div with an id to refernce to print the total score to the page. working on writing a function to assign the buttons a value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     
 //selects "number" html div. Writes a random number between 19 and 120 to the div.
 var chosenNumber = $("#number").html(Math.floor((Math.random() * (120 - 19 + 1)) + 19));
+//Total score
+var totalScore = 0
 //Red Crystal random Value
 randomRed =  Math.floor((Math.random() * 12) + 1);
 //blue crystal random value
@@ -10,11 +12,18 @@ randomBlue =  Math.floor((Math.random() * 12) + 1);
 randomGreen =  Math.floor((Math.random() * 12) + 1);
 //purple crystal random value
 randomPurple =  Math.floor((Math.random() * 12) + 1);
-//empty button values
+//button values
 var redValue = [randomRed];
 var blueValue = [randomBlue];
 var greenValue = [randomGreen];
 var purpleValue = [randomPurple];
+
 console.log(redValue, blueValue, greenValue, purpleValue);
+
+//button functionality
+
+function red() {
+    $('red').val(redValue);
+}
 
 });
